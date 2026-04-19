73eb48505b7748e2f47eab32e5e2cc6af37bc373
Eunbi <eunbi9533@naver.com>
2026-04-19 17:18:51 +0900

    feat: WebSocket 설정 추가

---
+++ api/src/main/java/team/pickz/api/global/config/WebSocketConfig.java
@@ -0,0 +1,32 @@
+package team.pickz.api.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import team.pickz.api.domain.draft.websocket.interceptor.WebSocketAuthHandshakeInterceptor;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final WebSocketAuthHandshakeInterceptor authHandshakeInterceptor;
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-draft")
+                .setAllowedOriginPatterns("*")
+                .addInterceptors(authHandshakeInterceptor)
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+}

51e6cde1f29daee4bdc92fb84e6ab233ec129c88
Eunbi <eunbi9533@naver.com>
2026-04-24 18:50:09 +0900

    fix: 지원하지 않는 소셜 로그인 제공자 예외 코드 수정

--- api/src/main/java/team/pickz/api/global/auth/presentation/exception/UnsupportedProviderException.java
+++ api/src/main/java/team/pickz/api/global/auth/presentation/exception/UnsupportedProviderException.java
@@ -5,7 +5,7 @@ import team.pickz.api.global.exception.CustomException;
 public class UnsupportedProviderException extends CustomException {
 
     public UnsupportedProviderException() {
-        super(AuthExceptionCode.INVALID_AUTH_CODE);
+        super(AuthExceptionCode.UNSUPPORTED_PROVIDER);
     }
 
 }
